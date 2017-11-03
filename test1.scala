399d5ea98dc6e32bff4e852d87e319edb2629828
Joao Machado <machado@codacy.com>
2017-11-03 15:49:50 +0000

    Update test1.scala

--- test1.scala
+++ test1.scala
@@ -4,6 +4,7 @@ import scala.util.{Failure, Success, Try}
 object myObject {
   var cenas=123
   var another=1234124
+  var qualquerCoisa=Some("body once told me").getOrElse("-_-")
   def MyFuntion(bla: Int) {
     var ola = 123
     var adeus = 123
