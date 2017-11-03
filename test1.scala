3f92ce8c98f3d462a1c48d79ab51ab3059f6f512
Joao Machado <machado@codacy.com>
2017-11-03 16:58:51 +0000

    Update test1.scala

--- test1.scala
+++ test1.scala
@@ -4,6 +4,7 @@ import scala.util.{Failure, Success, Try}
 object myObject {
   var cenas=123
   var another=1234124
+  var wow=123
   def MyFuntion(bla: Int) {
     var ola = 123
     var adeus = 123
